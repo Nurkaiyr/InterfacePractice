49dc05c3a1b243dac051dc2898c51e5555bf468b
Nurkaiyr <yedige21@gmail.com>
2017-12-27 01:33:01 +0600

    Practice

---
+++ InterfacePractice/IStudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice
+{
+    interface IStudent
+    {
+        string Name { get; set; }
+        string FullName { get; set; }
+        int[] Grades { get; set; }
+        String GetName();
+        String GetFullName();
+        Double GetAvgGrade();
+
+    }
+}
---
+++ InterfacePractice/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice
+{
+    public class Student:IStudent
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int[] Grades { get; set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public double GetAvgGrade()
+        {
+            int grades = Grades.Length;
+            double summa = 0;
+            foreach (var grade in Grades)
+            {
+                summa += grade;
+            }
+
+            return summa / grades;
+        }
+
+
+    }
+}
